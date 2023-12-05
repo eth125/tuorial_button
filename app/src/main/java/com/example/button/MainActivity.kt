6597016059ed2eb89d33a2b9668fe00230590c11
eth125 <siaethan125@gmail.com>
2023-12-05 13:10:48 +0800

    now doing 15

--- app/src/main/java/com/example/button/MainActivity.kt
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.button
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var userInput: EditText?=null
+    private var button : Button? = null
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
